eb299d623b13f7c69a6d1da657b456c10f99458a
noob <coniferchina@gmail.com>
2021-02-01 19:10:33 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="font-bold">Header</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {}
+})
+export default class Header extends Vue {
+  msg!: string
+}
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ambre-header></ambre-header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {}
+})
+export default class Index extends Vue {}
+</script>
